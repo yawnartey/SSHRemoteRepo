8b428a6d4d43a421cbb819edf15b452edebe1f0e
yawnartey <140964217+yawnartey@users.noreply.github.com>
2023-07-30 18:23:44 +0200

    more indentation has been made to index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 print("hello world!")
 print("this file has also been updated remotely")
-
 # add more lines to this file
-
 print("more lines will be added shortly")
+#more indentation has been made
