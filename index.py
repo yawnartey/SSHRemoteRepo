9980f73b8adb77c38f1fe1449db9ddbfd5a6a11a
yaw <yawenochnartey@gmail.com>
2023-07-30 18:43:40 +0200

    added another line locally

--- index.py
+++ index.py
@@ -3,3 +3,4 @@ print("this file has also been updated remotely")
 # add more lines to this file
 print("more lines will be added shortly")
 #more indentation has been made
+print ("this is to resolve mering conflicts") #added at local
